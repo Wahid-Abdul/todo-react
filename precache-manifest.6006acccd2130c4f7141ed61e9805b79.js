5c541d46459795a61f30229f20ab2520c96ab836
Wahid-Abdul <awahidag@gmail.com>
2019-12-02 23:05:56 +0530

    Updates

---
+++ precache-manifest.6006acccd2130c4f7141ed61e9805b79.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57c42c5d7ba9f94292ca58a6a5b12468",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "6b6b160c55dcc2cd33d1",
+    "url": "/todo-react/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c762ce42405008e39dc6",
+    "url": "/todo-react/static/js/2.c7038710.chunk.js"
+  },
+  {
+    "revision": "6b6b160c55dcc2cd33d1",
+    "url": "/todo-react/static/js/main.20607f20.chunk.js"
+  },
+  {
+    "revision": "68a51f12929e3f69cb59",
+    "url": "/todo-react/static/js/runtime-main.f5fc969b.js"
+  }
+]);
